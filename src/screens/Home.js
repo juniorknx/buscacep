948f97bbce731b924c94720d5247bd554265335e
Julio Junior <juniorknx@hotmail.com>
2024-08-22 15:50:50 -0300

    finalizando set up de stack navigator

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,8 +1,8 @@
 import { StatusBar } from "expo-status-bar";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 import { globalStyles, useGlobalFonts } from "../styles/globalStyle";
 
-export function HomeScreen() {
+export function HomeScreen({ navigation }) {
     const fontsLoaded = useGlobalFonts();
 
     if (!fontsLoaded) {
@@ -12,6 +12,7 @@ export function HomeScreen() {
     return (
         <View style={globalStyles.container}>
             <Text style={styles.title}>Home Page!!!</Text>
+            <Button title="Ir para Sobre" onPress={() => navigation.navigate('Sobre')} />
             <StatusBar style="auto" />
         </View>
     )
