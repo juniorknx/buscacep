93adb8409c05d25976d3e53b223b9ac5e3cea05a
Julio Junior <juniorknx@hotmail.com>
2024-08-26 16:58:59 -0300

    add content

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
-import { View, Text, StyleSheet, Button, TouchableOpacity, Keyboard, SafeAreaView } from "react-native";
-import { globalStyles, useGlobalFonts } from "../styles/globalStyle";
+import { View, Text, StyleSheet, TouchableOpacity, Keyboard, SafeAreaView, ActivityIndicator } from "react-native";
+import { useGlobalFonts } from "../styles/globalStyle";
 import { Input } from "../components/Input";
 import { Header } from "../components/Header";
 import { useEffect, useState } from "react";
@@ -10,6 +10,7 @@ import { api } from '../services/api'
 export function HomeScreen({ navigation }) {
     const [cep, setCep] = useState('')
     const [city, setCity] = useState('')
+    const [loading, setLoading] = useState(false)
     const fontsLoaded = useGlobalFonts();
 
     useEffect(() => {
@@ -23,17 +24,19 @@ export function HomeScreen({ navigation }) {
     }
 
     async function handleSearch() {
-        console.log('Hadle search')
+        console.log('Handle search')
         if (cep == '') {
             alert('Digite um CEP Válido!')
         }
         try {
+            setLoading(true)
             const response = await api.get(`${cep}/json`)
             setCity(response.data)
             console.log('FROM CITY ===>', city)
         } catch (err) {
             console.error(err, 'ERRO AO CHAMAR API DE CEP')
         } finally {
+            setLoading(false)
             Keyboard.dismiss()
         }
     }
@@ -46,7 +49,7 @@ export function HomeScreen({ navigation }) {
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.content}>
-                <StatusBar style="auto" />
+                <StatusBar style="auto" backgroundColor="#61dafb" />
                 <Header />
                 <View style={styles.inputView}>
                     <Input
@@ -59,15 +62,45 @@ export function HomeScreen({ navigation }) {
 
                 <View style={styles.btnArea}>
                     <TouchableOpacity style={styles.button} onPress={handleSearch}>
-                        <Text style={styles.buttonText}>Buscar</Text>
+                        <Text style={styles.buttonText}>
+                            {loading ? <ActivityIndicator size="small" /> : 'Buscar'}
+                        </Text>
                     </TouchableOpacity>
                 </View>
 
-                <View style={styles.resultBox}>
-                    <View style={styles.resultContainer}>
-                        <Text>B</Text>
+                {city &&
+                    <View style={styles.resultBox}>
+                        <View style={styles.resultContainer}>
+                            <Text style={styles.resultTitle}>Localidade: </Text>
+                            <Text style={styles.resultContent}>{city.localidade}</Text>
+                        </View>
+
+                        <View style={styles.resultContainer}>
+                            <Text style={styles.resultTitle}>Logradouro: </Text>
+                            <Text style={styles.resultContent}>{city.logradouro}</Text>
+                        </View>
+
+                        <View style={styles.resultContainer}>
+                            <Text style={styles.resultTitle}>Complemento: </Text>
+                            <Text style={styles.resultContent}>{city.complemento}</Text>
+                        </View>
+
+                        <View style={styles.resultContainer}>
+                            <Text style={styles.resultTitle}>Bairro: </Text>
+                            <Text style={styles.resultContent}>{city.bairro}</Text>
+                        </View>
+
+                        <View style={styles.resultContainer}>
+                            <Text style={styles.resultTitle}>UF: </Text>
+                            <Text style={styles.resultContent}>{city.uf}</Text>
+                        </View>
+
+                        <View style={styles.resultContainer}>
+                            <Text style={styles.resultTitle}>CEP: </Text>
+                            <Text style={styles.resultContent}>{city.cep}</Text>
+                        </View>
                     </View>
-                </View>
+                }
             </View>
         </SafeAreaView>
     )
@@ -110,5 +143,25 @@ const styles = StyleSheet.create({
         fontFamily: 'Poppins-Regular',
         color: '#fff',
         fontSize: 15
+    },
+
+    resultBox: {
+        alignItems: 'flex-start',
+        justifyContent: 'space-around',
+        backgroundColor: "#fff",
+        width: 355,
+        height: 350,
+        borderRadius: 10,
+        padding: 12
+    },
+
+    resultTitle: {
+        fontSize: 16,
+        fontFamily: 'Poppins-Bold'
+    },
+
+    resultContent: {
+        fontSize: 14,
+        fontFamily: 'Poppins-Light'
     }
 });
